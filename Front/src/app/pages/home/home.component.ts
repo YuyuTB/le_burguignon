1ea9f7366c2744292021591765df3d169129679f
Théo Berry <theo.berry.meleck@gmail.com>
2023-10-31 19:47:37 +0100

    Début Homepage + ajustements

--- Front/src/app/pages/home/home.component.ts
+++ Front/src/app/pages/home/home.component.ts
@@ -1,3 +1,5 @@
+
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  faStar = faStar;
 }
