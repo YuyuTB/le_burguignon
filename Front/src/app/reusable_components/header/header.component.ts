d6638750bbce7d7a345cbd6d47f9a0cd089235a4
YuyuTB <theo.berry.meleck@gmail.com>
2024-01-05 15:41:12 +0100

    Header tablette

--- Front/src/app/reusable_components/header/header.component.ts
+++ Front/src/app/reusable_components/header/header.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+	isMobile: boolean = false;
 
+	ngOnInit() {
+		// Example: You might use a service to get information about the device
+		this.isMobile = window.innerWidth < 900;
+
+		// Listen for window resize events to update isMobile dynamically
+		window.addEventListener('resize', () => {
+			this.isMobile = window.innerWidth < 900;
+		});
+	}
 }
