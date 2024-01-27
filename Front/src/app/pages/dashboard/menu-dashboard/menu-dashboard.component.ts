19a4434586366c21ff48687da4ef8e8c8d1cba7a
YuyuTB <theo.berry.meleck@gmail.com>
2024-01-27 15:40:37 +0100

    Ajout bouton pour ouvrir et fermer le sous menu dans la cat

--- Front/src/app/pages/dashboard/menu-dashboard/menu-dashboard.component.ts
+++ Front/src/app/pages/dashboard/menu-dashboard/menu-dashboard.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { faSquareCaretDown } from '@fortawesome/free-solid-svg-icons';
 @Component({
-  selector: 'app-menu-dashboard',
-  templateUrl: './menu-dashboard.component.html',
-  styleUrls: ['./menu-dashboard.component.scss']
+	selector: 'app-menu-dashboard',
+	templateUrl: './menu-dashboard.component.html',
+	styleUrls: ['./menu-dashboard.component.scss'],
 })
 export class MenuDashboardComponent {
-
+	faSquareCaretDown = faSquareCaretDown;
+	isOpen = false;
+	openHeader(): void {
+		if (this.isOpen) {
+			this.isOpen = false;
+		} else {
+			this.isOpen = true;
+		}
+	}
 }
