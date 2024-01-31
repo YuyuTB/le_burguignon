0c5d4d924661fc167efa79b97b236770b57b0151
YuyuTB <theo.berry.meleck@gmail.com>
2024-01-31 16:08:28 +0100

    Envoi email automatique après formulaire

--- Back/controllers/contact_controller.js
+++ Back/controllers/contact_controller.js
@@ -1,4 +1,7 @@
+const { PASSWORD } = require('../config/config');
 const Item = require('../models/contact');
+const pass = require('../config/password');
+const nodemailer = require('nodemailer');
 
 const itemController = {
 	getAllItems: async (req, res) => {
@@ -24,19 +27,16 @@ const itemController = {
 			}
 		} catch (error) {
 			console.error(error);
-			res.status(500).send(
-				"Erreur lors de la récupération de l'élément."
-			);
+			res.status(500).send("Erreur lors de la récupération de l'élément.");
 		}
 	},
 
 	createItem: async (req, res) => {
+		console.log(pass.pass);
 		try {
 			const { firstName, lastName, email, message } = req.body;
 			if (!firstName || !lastName || !email || !message) {
-				return res
-					.status(400)
-					.send('Tous les champs doivent être remplis.');
+				return res.status(400).send('Tous les champs doivent être remplis.');
 			}
 			const newItem = await Item.create({
 				firstName,
@@ -46,11 +46,39 @@ const itemController = {
 			});
 
 			res.status(201).json(newItem);
+			const transporter = nodemailer.createTransport({
+				service: 'gmail',
+				auth: {
+					user: 'theo.berry.meleck@gmail.com',
+					pass: pass.pass,
+				},
+			});
+
+			const emailContent = `
+				Prénom : ${firstName},
+				Nom : ${lastName},
+				Email : ${email},
+				Message : ${message}
+			`;
+			// Configurer le contenu de l'e-mail
+			const mailOptions = {
+				from: 'theo.berry.meleck@gmail.com',
+				to: 'theo.berry.meleck@gmail.com', // Remplacez par votre adresse e-mail
+				subject:
+					'Nouveau message depuis le formulaire de contact Burguignon.fr',
+				text: emailContent,
+			};
+
+			// Envoyer l'e-mail
+			transporter.sendMail(mailOptions, (error, info) => {
+				if (error) {
+					return console.log(error);
+				}
+				console.log('E-mail envoyé : ' + info.response);
+			});
 		} catch (error) {
 			console.error(error);
-			res.status(500).send(
-				"Erreur lors de la création d'un nouvel item."
-			);
+			res.status(500).send("Erreur lors de la création d'un nouvel item.");
 		}
 	},
 
@@ -64,9 +92,11 @@ const itemController = {
 				.catch((error) => res.status(401).json({ error }));
 		} catch (error) {
 			console.error(error);
-			res.status(500).send(
-				`Erreur lors de la suppression de l'item avec l'ID ${Contact_id}.`
-			);
+			res
+				.status(500)
+				.send(
+					`Erreur lors de la suppression de l'item avec l'ID ${Contact_id}.`
+				);
 		}
 	},
 };
